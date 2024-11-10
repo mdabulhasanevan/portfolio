0ca3c435026b788c2c9fb3af8f1be157da0944b4
unknown <evan.doict@gmail.com>
2024-11-10 16:00:38 +0600

    projects

--- app/project/page.js
+++ app/project/page.js
@@ -16,7 +16,7 @@ console.log("my data: " + projects);
          <title>Project | Portfolio</title>
         <section className="container mx-auto px-4">
           <h2 className="text-4xl font-extrabold text-center mb-10 text-blue-400">My Projects</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
             {projects.map((project) => (
               <div key={project.id} className="bg-gray-800 rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
                 <img src={project.photo} alt={project.name} className="w-full h-48 object-cover" />
@@ -25,9 +25,15 @@ console.log("my data: " + projects);
                   <p className="text-gray-400 mb-4">{project.discription}</p>
                   <a
                     href={project.link}
-                    className="inline-block px-4 py-2 mt-4 text-sm font-semibold text-white bg-blue-500 rounded hover:bg-blue-400 transition-colors"
+                    className="inline-block px-2 py-2 mt-4 text-sm font-semibold text-white bg-pink-500 rounded hover:bg-blue-400 transition-colors"
                   >
-                    View Project
+                    Live
+                  </a>
+                  <a
+                    href={project.github}
+                    className="inline-block px-2 py-2 mt-4 m-4 text-sm font-semibold text-white bg-blue-500 rounded hover:bg-blue-400 transition-colors"
+                  >
+                    View Code
                   </a>
                 </div>
               </div>
