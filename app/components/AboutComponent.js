0b3311c807bffe73b51c9a36793616d7a01dfbfd
unknown <evan.doict@gmail.com>
2024-11-11 15:55:39 +0600

    profile

--- app/components/AboutComponent.js
+++ app/components/AboutComponent.js
@@ -18,47 +18,47 @@ import ContactComponent from './contactComponent';
 
 
 
-const AboutComponent = ({userData, education, course, skill, experience}) => {
-     // Reference for the page content
+const AboutComponent = ({ userData, education, course, skill, experience }) => {
+  // Reference for the page content
   const contentRef = useRef();
-    return (
-        <div>
-            <section className="container mx-auto px-4 lg:px-8  " ref={contentRef} >
+  return (
+    <div ref={contentRef}>
+      <section className="container mx-auto px-4 lg:px-8  "  >
 
-            <div className="flex flex-col lg:flex-row items-stretch lg:space-x-5 space-y-4 lg:space-y-0">
-  {/* Profile Image */}<DownloadPage contentRef={contentRef} className="text-left" />
-  <div className="lg:w-1/3 flex flex-col justify-center bg-gray-800 items-center h-full min-h-full rounded-lg">
-  
-    <Image
-      src={userData.profileImage}
-      width={250}
-      height={250}
-      alt="Profile Picture"
-      className="rounded-full shadow-lg object-cover my-3"
-    />
-    <h3 className="text-3xl font-bold mb-4 text-center">{userData.name}</h3>
-    <p className="text-lg text-center text-gray-300 leading-relaxed mb-6">
-      {userData.bio}
-    </p>
-    
-    <ContactComponent />
-    <Education education={education} />
-  </div>
+        <div className="flex flex-col lg:flex-row items-stretch lg:space-x-5 space-y-4 lg:space-y-0">
+          {/* Profile Image */}<DownloadPage contentRef={contentRef} className="text-left" />
+          <div className="lg:w-1/3 flex flex-col justify-center bg-gray-800 items-center h-full min-h-full rounded-lg">
 
-  {/* Bio Section */}
-  <div className="lg:w-2/3 bg-gray-800 rounded-lg h-full min-h-full text-center lg:text-left">
- 
-    <Experience experiences={experience} />
-    <Skills skills={skill} />
-    <Course courses={course} />
-  </div>
-</div>
+            <img
+              src={userData.profileImage}
+              width={150}
+              height={150}
+              alt="Profile Picture"
+              className="rounded-full shadow-lg object-cover my-3"
+            />
+            <h3 className="text-3xl font-bold mb-4 text-center">{userData.name}</h3>
+            <p className="text-lg text-center text-gray-300 leading-relaxed mb-6">
+              {userData.bio}
+            </p>
 
+            <ContactComponent />
+            <Education education={education} />
+          </div>
 
+          {/* Bio Section */}
+          <div className="lg:w-2/3 bg-gray-800 rounded-lg h-full min-h-full text-center lg:text-left">
 
-            </section>
+            <Experience experiences={experience} />
+            <Skills skills={skill} />
+            <Course courses={course} />
+          </div>
         </div>
-    );
+
+
+
+      </section>
+    </div>
+  );
 };
 
 export default AboutComponent;
--- app/components/Education.js
+++ app/components/Education.js
@@ -10,8 +10,8 @@ const Education = async ({ education }) => {
       <h2 className="text-3xl font-bold text-center mb-2">Education</h2>
       <div className="education-list  max-w-4xl mx-auto px-4">
         {education.map((edu, index) => (
-          <div key={index} className="education-card mb-1 p-4  rounded-lg shadow-md">
-            <h3 className="text-xl text-yellow-400 font-semibold">{edu.degree}</h3>
+          <div key={index} className="education-card mb-1 p-2  rounded-lg shadow-md">
+            <h3 className="text-lg text-yellow-400 font-semibold">{edu.degree}</h3>
             <p className="text-orange-500">{edu.institute}</p>
             <p className="text-sm text-pink-500">
               {edu.startDate} - {edu.EndDate}
